a0a70ca542afacdfc9ebe8c34c77c2832670cd75
ZiadElraggal <161850748+ZiadElraggal@users.noreply.github.com>
2024-10-14 02:30:49 -0230

    Deployment

---
+++ server/chunks/build/client.manifest.mjs
@@ -0,0 +1,166 @@
+const client_manifest = {
+  "assets/AG.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "AG.BxMbV1nh.svg",
+    "src": "assets/AG.svg"
+  },
+  "assets/MI.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "MI.DfOfp5B1.svg",
+    "src": "assets/MI.svg"
+  },
+  "assets/header-background copy.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "header-background copy.BAxfBAAH.jpg",
+    "src": "assets/header-background copy.jpg"
+  },
+  "assets/profile-picture.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "profile-picture.E-aNUnnH.jpg",
+    "src": "assets/profile-picture.jpg"
+  },
+  "node_modules/@nuxt/icon/dist/runtime/components/index.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DpzFUHUn.js",
+    "name": "index",
+    "src": "node_modules/@nuxt/icon/dist/runtime/components/index.js",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BRrhnfRI.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-404.ygbHJO5Q.css"
+    ]
+  },
+  "error-404.ygbHJO5Q.css": {
+    "file": "error-404.ygbHJO5Q.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CO3lGv8z.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.B11Ibp8J.css"
+    ]
+  },
+  "error-500.B11Ibp8J.css": {
+    "file": "error-500.B11Ibp8J.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "UaibKLtt.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.DwSf3D-V.css"
+    ]
+  },
+  "entry.DwSf3D-V.css": {
+    "file": "entry.DwSf3D-V.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "C4-BjUax.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/@nuxt/icon/dist/runtime/components/index.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "index.DoET11KM.css"
+    ],
+    "assets": [
+      "profile-picture.E-aNUnnH.jpg",
+      "AG.BxMbV1nh.svg",
+      "MI.DfOfp5B1.svg",
+      "header-background copy.BAxfBAAH.jpg"
+    ]
+  },
+  "index.DoET11KM.css": {
+    "file": "index.DoET11KM.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "profile-picture.E-aNUnnH.jpg": {
+    "file": "profile-picture.E-aNUnnH.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "AG.BxMbV1nh.svg": {
+    "file": "AG.BxMbV1nh.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "MI.DfOfp5B1.svg": {
+    "file": "MI.DfOfp5B1.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "header-background copy.BAxfBAAH.jpg": {
+    "file": "header-background copy.BAxfBAAH.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
